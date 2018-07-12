874271bd77ef0f1e5a4311045956075141da25d0
tim.ivaikin <t.s.ivaykin@gmail.com>
2018-07-12 18:10:50 +0300

    POST user

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 app.get('/', function (req, res) {
   // res.send('Hello World!');
